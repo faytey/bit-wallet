e6635299ac0130e412d4ad706805afa753eb517a
Faith M. Roberts <faithroberts708@gmail.com>
2024-01-28 00:50:08 +0100

    bitcoin wallet

---
+++ create.js
@@ -0,0 +1,25 @@
+const bip32 = require("bip32");
+const bip39 = require("bip39");
+const bitcoin = require("bitcoinjs-lib");
+
+const network = bitcoin.networks.bitcoin;
+
+const path = `m/44'/1'/0'/0`;
+let mnemonic = bip39.generateMnemonic();
+let seed = bip39.mnemonicToSeedSync(mnemonic);
+let root = bip32.fromSeed(seed, network);
+
+let account = root.derivePath(path);
+let node = account.derive(0).derive(0);
+
+let btcAdd = bitcoin.payments.p2pkh({
+  pubkey: node.publicKey,
+  network: network,
+}).address;
+
+console.log(`
+Bitcoin Wallet Generated:
+- Address: ${btcAdd},
+- Key: ${node.toWIF()},
+- Mnemonic: ${mnemonic}
+`);
